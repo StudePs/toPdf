312ba831cdd132f26a2aa440450d961d2fb9aebd
ZFRenato <renatopscarlos@gmail.com>
2022-10-10 08:38:42 -0300

    feat: pdf file

--- src/util/index.js
+++ src/util/index.js
@@ -19,6 +19,10 @@ const organizer = (history) => {
                     timeLine.push(formatMsg('user', eventTime.message.text, eventTime.time))
                 }
             }
+            else if (Object.keys(eventTime.message)[0] == 'file') {
+                let aux = `<a href= '${eventTime.message.file.url}' target='_blanck'>${eventTime.message.file.url}</a>`;
+                timeLine.push(formatMsg('user', aux, eventTime.time))
+            }
         } else {
             if (typeof(eventTime.message) == 'string' || Object.keys(eventTime.message)[0] == 'text') {
                 if (typeof(eventTime.message) == 'string') {
@@ -34,6 +38,14 @@ const organizer = (history) => {
                     else if (msg.default.type === 'select') {
                         let aux = `${msg.default.payload.pick}<br> <ol>`;
                         msg.default.payload.options.forEach(option => {
+                            aux =  aux + `<li>${option.title}</li>`
+                        })
+                        aux = aux + ` </ol>`
+                        timeLine.push(formatMsg('bot', aux, eventTime.time))
+                    }
+                    else if (msg.default.type === 'quick_replies') {
+                        let aux = `${msg.default.payload.pick}<br> <ol>`;
+                        msg.default.payload.replies.forEach(option => {
                             aux =  aux + `<li>${option.title}</li>`
                         })
                         aux = aux + ` </ol>`
